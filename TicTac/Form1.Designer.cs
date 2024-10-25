18e037fca92b0ff2dc88ae702c6e3b12761a35cb
Admin <Admin@RAMOS>
2024-10-24 19:27:22 -0500

    Agregar archivos de proyecto.

---
+++ TicTac/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace TicTac
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btnReiniciar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.SystemColors.Control;
+            this.btn1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn1.BackgroundImage")));
+            this.btn1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn1.Location = new System.Drawing.Point(16, 18);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(118, 111);
+            this.btn1.TabIndex = 0;
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.BackColor = System.Drawing.SystemColors.Control;
+            this.btn2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn2.BackgroundImage")));
+            this.btn2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn2.Location = new System.Drawing.Point(152, 18);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(118, 111);
+            this.btn2.TabIndex = 1;
+            this.btn2.UseVisualStyleBackColor = false;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.BackColor = System.Drawing.SystemColors.Control;
+            this.btn3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn3.BackgroundImage")));
+            this.btn3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn3.Location = new System.Drawing.Point(289, 18);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(118, 111);
+            this.btn3.TabIndex = 2;
+            this.btn3.UseVisualStyleBackColor = false;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.BackColor = System.Drawing.SystemColors.Control;
+            this.btn4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn4.BackgroundImage")));
+            this.btn4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn4.Location = new System.Drawing.Point(16, 152);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(118, 111);
+            this.btn4.TabIndex = 3;
+            this.btn4.UseVisualStyleBackColor = false;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.BackColor = System.Drawing.SystemColors.Control;
+            this.btn5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn5.BackgroundImage")));
+            this.btn5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn5.Location = new System.Drawing.Point(152, 152);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(118, 111);
+            this.btn5.TabIndex = 4;
+            this.btn5.UseVisualStyleBackColor = false;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.BackColor = System.Drawing.SystemColors.Control;
+            this.btn6.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn6.BackgroundImage")));
+            this.btn6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn6.Location = new System.Drawing.Point(289, 152);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(118, 111);
+            this.btn6.TabIndex = 5;
+            this.btn6.UseVisualStyleBackColor = false;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.BackColor = System.Drawing.SystemColors.Control;
+            this.btn9.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn9.BackgroundImage")));
+            this.btn9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn9.Location = new System.Drawing.Point(289, 283);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(118, 111);
+            this.btn9.TabIndex = 8;
+            this.btn9.UseVisualStyleBackColor = false;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.BackColor = System.Drawing.SystemColors.Control;
+            this.btn8.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn8.BackgroundImage")));
+            this.btn8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn8.Location = new System.Drawing.Point(152, 283);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(118, 111);
+            this.btn8.TabIndex = 7;
+            this.btn8.UseVisualStyleBackColor = false;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.BackColor = System.Drawing.SystemColors.Control;
+            this.btn7.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn7.BackgroundImage")));
+            this.btn7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn7.Location = new System.Drawing.Point(16, 283);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(118, 111);
+            this.btn7.TabIndex = 6;
+            this.btn7.UseVisualStyleBackColor = false;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btnReiniciar
+            // 
+            this.btnReiniciar.BackColor = System.Drawing.Color.Transparent;
+            this.btnReiniciar.BackgroundImage = global::TicTac.Properties.Resources.Reset;
+            this.btnReiniciar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnReiniciar.ForeColor = System.Drawing.Color.Transparent;
+            this.btnReiniciar.Location = new System.Drawing.Point(152, 400);
+            this.btnReiniciar.Name = "btnReiniciar";
+            this.btnReiniciar.Size = new System.Drawing.Size(118, 44);
+            this.btnReiniciar.TabIndex = 9;
+            this.btnReiniciar.UseVisualStyleBackColor = false;
+            this.btnReiniciar.Click += new System.EventHandler(this.btnReiniciar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::TicTac.Properties.Resources.Fondo;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnReiniciar);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btnReiniciar;
+    }
+}
+
---
+++ TicTac/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTac
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private bool turnoPikachu = true; // true para Pikachu, false para Charizard
+        private bool juegoActivo = true; // Indica si el juego está activo
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            RealizarMovimiento(btn1);
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            RealizarMovimiento(btn2);
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            RealizarMovimiento(btn3);
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            RealizarMovimiento(btn4);
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            RealizarMovimiento(btn5);
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            RealizarMovimiento(btn6);
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            RealizarMovimiento(btn7);
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            RealizarMovimiento(btn8);
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            RealizarMovimiento(btn9);
+        }
+
+        private void btnReiniciar_Click(object sender, EventArgs e)
+        {
+            ReiniciarJuego();
+        }
+
+        private void RealizarMovimiento(Button boton)
+        {
+            if (!juegoActivo || boton.Tag != null) return; // Ignorar si el juego no está activo o el botón ya está ocupado
+
+            if (turnoPikachu)
+            {
+                boton.BackgroundImage = Properties.Resources.f_1;
+                boton.Tag = "Pikachu";
+            }
+            else
+            {
+                boton.BackgroundImage = Properties.Resources.f_2;
+                boton.Tag = "Charizard";
+            }
+
+            turnoPikachu = !turnoPikachu; // Cambiar el turno
+            VerificarGanador();
+        }
+
+        private void VerificarGanador()
+        {
+            // Listas de combinaciones ganadoras (posiciones en filas, columnas y diagonales)
+            if (EsGanador(btn1, btn2, btn3) || // fila 1
+                EsGanador(btn4, btn5, btn6) || // fila 2
+                EsGanador(btn7, btn8, btn9) || // fila 3
+                EsGanador(btn1, btn4, btn7) || // columna 1
+                EsGanador(btn2, btn5, btn8) || // columna 2
+                EsGanador(btn3, btn6, btn9) || // columna 3
+                EsGanador(btn1, btn5, btn9) || // diagonal principal
+                EsGanador(btn3, btn5, btn7))   // diagonal inversa
+            {
+                // Si hay un ganador, mostrar quién ganó y desactivar los botones
+                string ganador = turnoPikachu ? "Charizard" : "Pikachu";
+                MessageBox.Show($"{ganador} es el ganador!", "¡Felicidades!");
+
+                DesactivarBotones();
+            }
+        }
+
+        private bool EsGanador(Button b1, Button b2, Button b3)
+        {
+            // Verificar que los tres botones tengan el mismo Tag y no sean null
+            if (b1.Tag != null && b2.Tag != null && b3.Tag != null)
+            {
+                if (b1.Tag.ToString() == b2.Tag.ToString() &&
+                    b2.Tag.ToString() == b3.Tag.ToString())
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void DesactivarBotones()
+        {
+            // Desactivar todos los botones después de que hay un ganador
+            juegoActivo = false; // Cambia el estado del juego a inactivo
+            btn1.Enabled = false;
+            btn2.Enabled = false;
+            btn3.Enabled = false;
+            btn4.Enabled = false;
+            btn5.Enabled = false;
+            btn6.Enabled = false;
+            btn7.Enabled = false;
+            btn8.Enabled = false;
+            btn9.Enabled = false;
+        }
+
+        private void ReiniciarJuego()
+        {
+            // Reiniciar la imagen de todos los botones a la Pokebola
+            btn1.BackgroundImage = Properties.Resources.f_0;
+            btn2.BackgroundImage = Properties.Resources.f_0;
+            btn3.BackgroundImage = Properties.Resources.f_0;
+            btn4.BackgroundImage = Properties.Resources.f_0;
+            btn5.BackgroundImage = Properties.Resources.f_0;
+            btn6.BackgroundImage = Properties.Resources.f_0;
+            btn7.BackgroundImage = Properties.Resources.f_0;
+            btn8.BackgroundImage = Properties.Resources.f_0;
+            btn9.BackgroundImage = Properties.Resources.f_0;
+
+            // Reactivar los botones
+            btn1.Enabled = true;
+            btn2.Enabled = true;
+            btn3.Enabled = true;
+            btn4.Enabled = true;
+            btn5.Enabled = true;
+            btn6.Enabled = true;
+            btn7.Enabled = true;
+            btn8.Enabled = true;
+            btn9.Enabled = true;
+
+            // Restablecer el estado de los botones
+            btn1.Tag = null;
+            btn2.Tag = null;
+            btn3.Tag = null;
+            btn4.Tag = null;
+            btn5.Tag = null;
+            btn6.Tag = null;
+            btn7.Tag = null;
+            btn8.Tag = null;
+            btn9.Tag = null;
+
+            // Reiniciar el turno a Pikachu
+            turnoPikachu = true;
+            juegoActivo = true; // Activar el juego nuevamente
+        }
+
+    }
+}
+
+
+
+    
+
